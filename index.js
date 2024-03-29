ab89685323c50b122f188d2e3f4541cf5bde241a
majadey4u <maryjaneogbuju@gmail.com>
2024-03-29 04:39:22 +0100

    new

---
+++ index.js
@@ -0,0 +1,36 @@
+What You're Aiming For
+
+Google Maps: An amazing tool used every day!
+
+Create a landing page from your choice (React app )
+Integrate google maps into your code.
+The bloc of Google maps should be centralized in the middle to be well visualized
+Use external libraries for design & good looking of your app
+
+Instructions
+
+Make your researches (Google, YouTube) to understand how it works. 
+
+This Link can help you.
+
+https://blog.logrocket.com/integrating-google-maps-react/
+
+
+How We'll Evaluate
+
+Your checkpoint will be evaluated based on these criteria, each rated from 0 to 5.
+
+What We're Looking For
+
+Google Map integration
+
+Use of comments during the code
+
+Use of external libraries
+
+Checkpoint Assessment
+
+Your Tech Skills
+Quality of Work
+Problem-Solving Skills
+Deadline Alert: (0 score if missed)
